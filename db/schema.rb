9dee15a3d732a97fb41da529bff32e007c17d1e3
Luis Pedraza <luispedraza@gmail.com>
2013-09-16 13:38:00 +0200

    Aliminada image_url de los productos, ahora están en la asociación product_images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130914144323) do
+ActiveRecord::Schema.define(version: 20130916113600) do
 
   create_table "cart_items", force: true do |t|
     t.integer  "product_id"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 20130914144323) do
   create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
-    t.string   "image_url"
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
