61f1be5829e2851e4938e794e7b9ae9c5863ac52
Luis Pedraza <luispedraza@gmail.com>
2013-09-17 14:05:32 +0200

    Creada vista de detalle del carrito en application.html.erb layout principal

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,7 @@ class StoreController < ApplicationController
   def index
   	# Lista de productos ordenados por su nombre
   	@products = Product.order(:name)
+  	# para que se pueda mostrar el carrito en el detalle
+  	@cart = current_cart
   end
 end
